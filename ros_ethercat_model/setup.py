50a472fe68a99df065180372ff38060b8da2307c
VahidAminZ <vahid@shadowrobot.com>
2015-09-18 14:29:04 +0100

    moved the library to src folder and related changes

--- ros_ethercat_model/setup.py
+++ ros_ethercat_model/setup.py
@@ -4,7 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['ros_ethercat_model'],
     scripts=[],
-    package_dir={'': 'scripts'}
+    package_dir={'': 'src'}
 )
 
 setup(**d)
