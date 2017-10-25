a8f84f466518a7c27aa5a59e4584bb3d9f1014db
dg-shadow <dg@shadowrobot.com>
2017-10-25 11:03:34 +0000

    comment

--- ros_ethercat_model/include/ros_ethercat_model/robot_state.hpp
+++ ros_ethercat_model/include/ros_ethercat_model/robot_state.hpp
@@ -8,7 +8,7 @@
  *
  *  Copyright (c) 2014, Shadow Robot Company Ltd.
  *  All rights reserved.
-# *
+ *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions
  *  are met:
