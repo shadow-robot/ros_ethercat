42a085783589b560afb0195263604f87af7f6280
dg-shadow <dg@shadowrobot.com>
2018-12-20 10:35:40 +0000

    moved interfaces to child class

--- ros_ethercat_model/include/ros_ethercat_model/ros_ethercat.hpp
+++ ros_ethercat_model/include/ros_ethercat_model/ros_ethercat.hpp
@@ -50,8 +50,6 @@
 #include <hardware_interface/robot_hw.h>
 #include <hardware_interface/joint_state_interface.h>
 #include <hardware_interface/joint_command_interface.h>
-#include <hardware_interface/actuator_state_interface.h>
-#include <hardware_interface/actuator_command_interface.h>
 #include <hardware_interface/imu_sensor_interface.h>
 #include <sr_robot_msgs/MechanismStatistics.h>
 #include "ros_ethercat_model/robot_state.hpp"
@@ -381,13 +379,9 @@ public:
   ptr_vector<EthercatHardware> ethercat_hardware_;
   boost::scoped_ptr<MechStatsPublisher> mech_stats_publisher_;
 
-  
   // robot state interface
   ros_ethercat_model::RobotStateInterface robot_state_interface_;
 
-  // actuator state interface
-  hardware_interface::ActuatorStateInterface actuator_state_interface_;
-  
   // joint state interface
   hardware_interface::JointStateInterface joint_state_interface_;
 
@@ -399,9 +393,6 @@ public:
   hardware_interface::VelocityJointInterface joint_velocity_command_interface_;
   hardware_interface::EffortJointInterface joint_effort_command_interface_;
 
-
-  // actuator command interface
-  hardware_interface::EffortActuatorInterface actuator_effort_command_interface_;
 protected:
   static int lock_fd(int fd)
   {
