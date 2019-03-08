abdee62e749f5a4f03b4884b823f8ae96fe90f29
Guillaume Walck <gwalck@techfak.uni-bielefeld.de>
2019-03-08 12:06:18 +0100

    Switched to Tinyxml2

--- ros_ethercat_model/include/ros_ethercat_model/robot_state.hpp
+++ ros_ethercat_model/include/ros_ethercat_model/robot_state.hpp
@@ -70,7 +70,7 @@ using std::string;
 class RobotState : public hardware_interface::HardwareInterface
 {
 public:
-  explicit RobotState(TiXmlElement *root = NULL, vector<string> joint_filter = vector<string>())
+  explicit RobotState(tinyxml2::XMLElement *root = NULL, vector<string> joint_filter = vector<string>())
     : joint_filter_(joint_filter), transmission_loader_("ros_ethercat_model", "ros_ethercat_model::Transmission")
   {
     if (root)
@@ -83,7 +83,7 @@ public:
     return &actuator_states_[name];
   }
 
-  void initXml(TiXmlElement *root)
+  void initXml(tinyxml2::XMLElement *root)
   {
     try
     {
@@ -107,7 +107,7 @@ public:
         }
       }
 
-      for (TiXmlElement *xit = root->FirstChildElement("transmission");
+      for (tinyxml2::XMLElement *xit = root->FirstChildElement("transmission");
            xit;
            xit = xit->NextSiblingElement("transmission"))
       {
