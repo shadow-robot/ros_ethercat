87522866e6ab2ecefa94f755be146452b16c2406
Ethan Fowler <ethan@shadowrobot.com>
2019-08-30 17:11:12 +0000

    Copyright fixes.

--- ros_ethercat_model/include/ros_ethercat_model/ros_ethercat.hpp
+++ ros_ethercat_model/include/ros_ethercat_model/ros_ethercat.hpp
@@ -1,41 +1,41 @@
 /*
- * ros_ethercat.hpp
- *
- *  Created on: 7 Jan 2014
- *      Author: Manos Nikolaidis
- *
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2014, Shadow Robot Company Ltd.
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of the Willow Garage nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *********************************************************************/
+* ros_ethercat.hpp
+*
+*  Created on: 7 Jan 2014
+*      Author: Manos Nikolaidis
+*
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2014, Shadow Robot Company Ltd.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
 
 #ifndef ROS_ETHERCAT_MODEL_ROS_ETHERCAT_HPP_
 #define ROS_ETHERCAT_MODEL_ROS_ETHERCAT_HPP_
@@ -59,22 +59,22 @@
 #include "ros_ethercat_model/imu_state.hpp"
 
 
-/** \brief Contains robot state information and init, read, write function.
- *
- * The robot state is contained in ros_ethercat_model::RobotStateState object
- * as used by pr2_controller object. eNvertheless, the main loop in main.cpp
- * instantiates a ros_control controller_manager. So a pr2_controller with few modifications
- * may be loaded with controller_manager with RobotState as a custom interface.
- *
- * ros_control interfaces are exposed alongside RobotState. So controllers from
- * ros_controllers package may also be loaded. These new interfaces contain pointers
- * to data in RobotState so there is no copying or data redundancy.
- *
- * The read and write functions will call the propagate functions of pr2_transmissions.
- * Hardware read and write takes place in the EthercatHardware object in main.cpp
- *
- * initXml, read and write should be called inside main.cpp
- */
+/* \brief Contains robot state information and init, read, write function.
+*
+* The robot state is contained in ros_ethercat_model::RobotStateState object
+* as used by pr2_controller object. eNvertheless, the main loop in main.cpp
+* instantiates a ros_control controller_manager. So a pr2_controller with few modifications
+* may be loaded with controller_manager with RobotState as a custom interface.
+*
+* ros_control interfaces are exposed alongside RobotState. So controllers from
+* ros_controllers package may also be loaded. These new interfaces contain pointers
+* to data in RobotState so there is no copying or data redundancy.
+*
+* The read and write functions will call the propagate functions of pr2_transmissions.
+* Hardware read and write takes place in the EthercatHardware object in main.cpp
+*
+* initXml, read and write should be called inside main.cpp
+*/
 
 #include <vector>
 #include <string>
