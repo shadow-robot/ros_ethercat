73e7a50f3d2af4d316fe89cdf1eb2668c2bf6e42
Kirsty <kirsty@shadowrobot.com>
2019-11-22 17:08:19 +0000

    moving command interface changes

--- ros_ethercat_model/include/ros_ethercat_model/hardware_interface.hpp
+++ ros_ethercat_model/include/ros_ethercat_model/hardware_interface.hpp
@@ -38,6 +38,7 @@
 #include <string>
 #include <vector>
 #include <hardware_interface/actuator_state_interface.h>
+#include <ros_ethercat_model/actuator_command_interface.h>
 
 #include <ros/ros.h>
 
@@ -62,7 +63,7 @@ public:
     max_effort_(0.0),
     motor_voltage_(0.0),
     flags_(0),
-    control_mode_(hardware_interface::PWM)
+    control_mode_(COMMAND_MODE_PWM)
   {
   }
 
@@ -89,7 +90,7 @@ public:
   double max_effort_;  //!< Absolute torque limit for actuator (derived from motor current limit). (in Nm)
 
   double motor_voltage_;  //!< Motor voltage (in volts)
-  hardware_interface::ActuatorCommandMode control_mode_;  // switch between pwm and effort
+  ActuatorCommandMode control_mode_;  // switch between pwm and effort
 };
 
 class ActuatorCommand
