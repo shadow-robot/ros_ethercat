497b2a7afa3be86066d4336615490c48c102b6fb
Chris <chris@shadowrobot.com>
2025-09-25 14:25:41 +0000

    license and linter

--- ros_ethercat_model/include/ros_ethercat_model/temperature_sensor_state.hpp
+++ ros_ethercat_model/include/ros_ethercat_model/temperature_sensor_state.hpp
@@ -1,7 +1,41 @@
 /*
-* Copyright (C) 2025 Shadow Robot Company Ltd - All Rights Reserved. Proprietary and Confidential.
-* Unauthorized copying of the content in this file, via any medium is strictly prohibited.
-*/
+* temperature_sensor_state.hpp
+*
+*  Created on: 25 Sept 2025
+*      Author: Chris White
+*
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2014, 2016, 2017, 2019, 2022, 2025 Shadow Robot Company Ltd.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
 
 /*
 * @file   temperature_sensor_state.h
@@ -19,10 +53,6 @@
 #include <string>
 #include <vector>
 #include <hardware_interface/internal/hardware_resource_manager.h>
-
-/* \brief This class provides the controllers with an interface to the reference temperature sensor states
-*
-*/
 
 using std::vector;
 using std::string;
@@ -50,8 +80,9 @@ public:
 
   string getName() const { return name_; }
 
+  uint16_t temperature_raw_;
   uint16_t temperature_;
-  
+
 private:
   string name_;
   size_t dimensions_;
